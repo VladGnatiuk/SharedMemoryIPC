24ad3cb67406964349b2ad04afc1adc29b07bccb
Vlad <vladyslav.gnatiuk@gmail.com>
2021-11-02 22:21:55 -0400

    Create empty projects

---
+++ src/SharedMemoryIPC.Benchmark/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SharedMemoryIPC.Benchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
